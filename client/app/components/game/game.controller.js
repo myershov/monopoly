662afeba57b13fe39ceccc38fa345659f101e0f7
Vlad <vlad.golyachenko@gmail.com>
2017-03-15 17:45:44 +0200

    edit main window game

--- client/app/components/game/game.controller.js
+++ client/app/components/game/game.controller.js
@@ -1,25 +1,27 @@
 class GameController {
   constructor() {
     this.name = 'Game';
-    var a={name: ''},
-        b={name: ''},
-        c={name: ''},
-        d={name: ''},
-        e={name: ''},
-        f={name: ''},
-        g={name: ''},
-        h={name: ''},
-        i={name: ''},
-        j={name: ''},
-        k={name: ''},
-        l={name: ''},
-        m={name: ''},
-        n={name: ''},
-        p={name: ''};
+    var a={name: 'Початок'},
+        b={name: 'Музей'},
+        c={name: 'Банк'},
+        d={name: 'Школа'},
+        e={name: 'Китнотеатр'},
+        f={name: 'Чернігівське'},
+        g={name: 'Парк'},
+        h={name: 'Аеропорт'},
+        i={name: 'Податкова'},
+        j={name: 'Планетарій'},
+        k={name: 'Укрзалізниця'},
+        l={name: 'Мтс'},
+        m={name: 'Театр'},
+        n={name: 'Київстар'},
+        p={name: 'Пропуск'},
+        o={name: 'Бонус'},
+        v={name: '+2 Ходи'};
     this.bloc = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,p];
-    this.left =[a,b,c,d,e,f,g];
-    this.right =[a,b,c,d,e,f,g];
-    this.bottom = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,p];
+    this.left = [l,b,c,d,e,f,g];
+    this.right = [l,b,c,d,e,f,g];
+    this.bottom = [v,b,c,d,e,f,g,h,i,j,k,l,m,n,o];
   }
 }
 
--- client/app/components/list/list.controller.js
+++ client/app/components/list/list.controller.js
@@ -1,10 +1,10 @@
 class ListController {
   constructor() {
     this.name = 'List';
-    var a={name: 'test3427'},
-        b={name: 'test3451'},
-        c={name: 'test662'};
-    this.bloc = [a,b,c];
+    var a={name: 'test1111' ,money:'343355', size:'2x6',nop:'5/5'},
+        b={name: 'test2222' ,money:'377735', size:'2x3',nop:'3/7'},
+        c={name: 'test3333' ,money:'343253', size:'2x8',nop:'6/8'};
+    this.allTopCells = [a,b,c];
   }
 }
 
