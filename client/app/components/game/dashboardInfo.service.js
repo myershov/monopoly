5e83241b8532d20345460ef0f5a2026f6cd22570
Mykola Yershov <my.yershov@gmail.com>
2017-03-18 14:21:01 +0200

    Logic update for move of player

---
+++ client/app/components/game/dashboardInfo.service.js
@@ -0,0 +1,151 @@
+const dashboard = {
+    top: {
+      cell_0: {
+        name: 'Movie',
+        positionX: 10,
+        positionY: -150
+      },
+      cell_1: {
+        name: 'Movie2342',
+        positionX: 10,
+        positionY: -50
+      },
+      cell_2: {
+        name: 'Movie234234',
+        positionX: 10,
+        positionY: 0
+      },
+      cell_3: {
+        name: 'Movie123sdf',
+        positionX: 10,
+        positionY: 50
+      },
+      cell_4: {
+        name: 'Moasdfsdfvie'
+      },
+      cell_5: {
+        name: 'Movisadf asdfa de'
+      },
+      cell_6: {
+        name: 'Moviasdf sdafe'
+      },
+      cell_7: {
+        name: 'Moviasdf asd e'
+      },
+      cell_8: {
+        name: 'Movias dfa dfe'
+      },
+      cell_9: {
+        name: 'Moviasdfas df sa fde'
+      },
+      cell_10: {
+        name: 'Moviasdfas df sa fde'
+      },
+      cell_11: {
+        name: 'Moviasdfas df sa fde'
+      },
+      cell_12: {
+        name: 'Moviasdfas df sa fde'
+      },
+      cell_13: {
+        name: 'Moviasdfas df sa fde'
+      },
+      cell_14: {
+        name: 'Moviasdfas df sa fde'
+      }
+    },
+    left: [
+      {
+        name: 'Movie'
+      },
+      {
+        name: 'Movie2342'
+      },
+      {
+        name: 'Movie234234'
+      },
+      {
+        name: 'Movie123sdf'
+      },
+      {
+        name: 'Moasdfsdfvie'
+      },
+      {
+        name: 'Movisadf asdfa de'
+      },
+      {
+        name: 'Moviasdf sdafe'
+      }
+    ],
+    right: [
+      {
+        name: 'Movie'
+      },
+      {
+        name: 'Movie2342'
+      },
+      {
+        name: 'Movie234234'
+      },
+      {
+        name: 'Movie123sdf'
+      },
+      {
+        name: 'Moasdfsdfvie'
+      },
+      {
+        name: 'Movisadf asdfa de'
+      },
+      {
+        name: 'Moviasdf sdafe'
+      }
+    ],
+    bottom: [
+      {
+        name: 'Movie'
+      },
+      {
+        name: 'Movie2342'
+      },
+      {
+        name: 'Movie234234'
+      },
+      {
+        name: 'Movie123sdf'
+      },
+      {
+        name: 'Moasdfsdfvie'
+      },
+      {
+        name: 'Movisadf asdfa de'
+      },
+      {
+        name: 'Moviasdf sdafe'
+      },
+      {
+        name: 'Moviasdf asd e'
+      },
+      {
+        name: 'Movias dfa dfe'
+      },
+      {
+        name: 'Moviasdfas df sa fde'
+      },
+      {
+        name: 'Moviasdfas df sa fde'
+      },
+      {
+        name: 'Moviasdfas df sa fde'
+      },
+      {
+        name: 'Moviasdfas df sa fde'
+      },
+      {
+        name: 'Moviasdfas df sa fde'
+      },
+      {
+        name: 'Moviasdfas df sa fde'
+      }
+    ]
+};
+export {dashboard};
--- client/app/components/game/game.controller.js
+++ client/app/components/game/game.controller.js
@@ -1,77 +1,66 @@
+import { dashboard } from './dashboardInfo.service'
+
 class GameController {
-  constructor() {
-    this.name = 'Game';
-    this.i = 10;
-    this.j = 160;
-    this.c = 0;
-    var a={name: 'Початок'},
-        b={name: 'Музей'},
-        c={name: 'Банк'},
-        d={name: 'Школа'},
-        e={name: 'Китнотеатр'},
-        f={name: 'Чернігівське'},
-        g={name: 'Парк'},
-        h={name: 'Аеропорт'},
-        i={name: 'Податкова'},
-        j={name: 'Планетарій'},
-        k={name: 'Укрзалізниця'},
-        l={name: 'Мтс'},
-        m={name: 'Театр'},
-        n={name: 'Київстар'},
-        p={name: 'Пропуск'},
-        o={name: 'Бонус'},
-        v={name: '+2 Ходи'};
-    this.bloc = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,p];
-    this.left = [l,b,c,d,e,f,g];
-    this.right = [l,b,c,d,e,f,g];
-    this.bottom = [v,b,c,d,e,f,g,h,i,j,k,l,m,n,o];
+    constructor() {
+        this.name = 'Game';
+        this.top = dashboard.top;
+        this.left = dashboard.left;
+        this.right = dashboard.right;
+        this.bottom = dashboard.bottom;
+        this.cellId = 0;
     }
-qwer () {
-    if(this.c < 13){
-        document.getElementById('check').style.marginLeft = this.i+'px';
-        this.i=this.i+50;
-        this.c++;
-    } else if(this.c === 13){
-        this.i=this.i+100;
-        document.getElementById('check').style.marginLeft = this.i+'px';
-        this.c++;
-    } else if(this.c < 21){
-        document.getElementById('check').style.marginTop = this.j+'px';
-        this.j=this.j+50;
-        this.c++;
-    } else if(this.c === 21){
-        this.j=this.j+100;
-        document.getElementById('check').style.marginTop = this.j+'px';
-        this.c++;
-    } else if(this.c === 22){
-        this.i=this.i-150;
-        document.getElementById('check').style.marginLeft = this.i+'px';
-        this.c++;
-    } else if(this.c < 35){
-        this.i=this.i-50;
-        document.getElementById('check').style.marginLeft = this.i+'px';
-        this.c++;
-    } else if(this.c === 35){
-        this.i=this.i-150;
-        document.getElementById('check').style.marginLeft = this.i+'px';
-        this.c++;
-    } else if(this.c === 36){
-        this.j=this.j-150;
-        document.getElementById('check').style.marginTop = this.j+'px';
-        this.c++;
-    } else if(this.c < 43){
-        this.j=this.j-50;
-        document.getElementById('check').style.marginTop = this.j+'px';
-        this.c++;
-    } else if(this.c === 43){
-        this.j=this.j-150;
-        document.getElementById('check').style.marginTop = this.j+'px';
-        this.c=0;
-        this.i = 10;
-        this.j = 160;
+//    qwer () {
+//    if(this.c < 13){
+//        document.getElementById('check').style.marginLeft = this.i+'px';
+//        this.i=this.i+50;
+//        this.c++;
+//    } else if(this.c === 13){
+//        this.i=this.i+100;
+//        document.getElementById('check').style.marginLeft = this.i+'px';
+//        this.c++;
+//    } else if(this.c < 21){
+//        document.getElementById('check').style.marginTop = this.j+'px';
+//        this.j=this.j+50;
+//        this.c++;
+//    } else if(this.c === 21){
+//        this.j=this.j+100;
+//        document.getElementById('check').style.marginTop = this.j+'px';
+//        this.c++;
+//    } else if(this.c === 22){
+//        this.i=this.i-150;
+//        document.getElementById('check').style.marginLeft = this.i+'px';
+//        this.c++;
+//    } else if(this.c < 35){
+//        this.i=this.i-50;
+//        document.getElementById('check').style.marginLeft = this.i+'px';
+//        this.c++;
+//    } else if(this.c === 35){
+//        this.i=this.i-150;
+//        document.getElementById('check').style.marginLeft = this.i+'px';
+//        this.c++;
+//    } else if(this.c === 36){
+//        this.j=this.j-150;
+//        document.getElementById('check').style.marginTop = this.j+'px';
+//        this.c++;
+//    } else if(this.c < 43){
+//        this.j=this.j-50;
+//        document.getElementById('check').style.marginTop = this.j+'px';
+//        this.c++;
+//    } else if(this.c === 43){
+//        this.j=this.j-150;
+//        document.getElementById('check').style.marginTop = this.j+'px';
+//        this.c=0;
+//        this.i = 10;
+//        this.j = 160;
+//    }
+//    };
+    changePlayerPosition () {
+      let tip = angular.element( document.querySelector( '#check' ) )[0];
+      let cellName = `cell_${this.cellId}`;
+      tip.style.marginTop = dashboard.top[cellName].positionX + 'px';
+      tip.style.marginLeft = dashboard.top[cellName].positionY + 'px';
+      this.cellId++;
     }
-    conslolelog(this.c);
-    };
 }
 
 export default GameController;
